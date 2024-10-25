6c31b15145e45df52e7815971ad3b9602921928b
anaplmiranda <anaoliveira886@gmail.com>
2024-10-25 20:11:42 -0300

    Novas rotas

--- app.py
+++ app.py
@@ -59,10 +59,9 @@ def home():
     return "Hello, Flask!"
 
 @app.route('/scrape/producao', methods=['GET'])
-
 @auth.login_required
 def scrape_producao():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
+    url = 'https://web.archive.org/web/20201203223441/http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
     if not url:
         return jsonify({"error": "URL é obrigatória"}), 400
     try:
@@ -90,7 +89,7 @@ def scrape_producao():
 @app.route('/scrape/comercializacao', methods=['GET'])
 @auth.login_required
 def scrape_comercializacao():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04'
+    url = 'https://web.archive.org/web/20201203231003/http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04'
     if not url:
         return jsonify({"error": "URL é obrigatória"}), 400
     try:
@@ -114,15 +113,275 @@ def scrape_comercializacao():
     except  Exception as e: 
         return  jsonify({"error": str(e)}), 500        
 
-@app.route('/items', methods=['GET'])
-def get_items():
-    return  jsonify(items)
 
-@app.route('/items', methods=['POST'])
-def create_items():
-    data = request.get_json()
-    items.append(data)
-    return jsonify(data), 201            
+@app.route('/scrape/importacao/<tipo>', methods=['GET'])
+@auth.login_required
+def scrape_importacao(tipo):
+    vinho_de_mesa = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_05'
+    espumantes = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_05'
+    uvas_frescas = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_05'
+    uvas_passas = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_05'
+    suco_de_uva = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_05&opcao=opt_05'
+    if tipo not in ['vinho_de_mesa', 'espumantes', 'uvas_frescas', 'uvas_passas', 'suco_de_uva']:
+        return jsonify({'error': 'Tipo de importação inválido'}), 400
+    match tipo:
+        case  "vinho_de_mesa":
+            response = requests.get(vinho_de_mesa)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "espumantes":
+            response = requests.get(espumantes)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "uvas_frescas":
+            response = requests.get(uvas_frescas)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "uvas_passas":
+            response = requests.get(uvas_passas)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "suco_de_uva":
+            response = requests.get(suco_de_uva)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+               
+@app.route('/scrape/processamento/<tipo_p>', methods=['GET'])
+@auth.login_required
+def scrape_processamento(tipo_p):
+    viniferas = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_03'
+    americanas_hibri = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_03'
+    uvas_mesa = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_03'
+    sem_classificacao = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_03'
+    if tipo_p not in ['viniferas', 'americanas_hibri', 'uvas_mesa', 'uvas_passas', 'sem_classificacao']:
+        return jsonify({'error': 'Tipo de importação inválido'}), 400
+    match tipo_p:
+        case  "viniferas":
+            response = requests.get(viniferas)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "americanas_hibri":
+            response = requests.get(americanas_hibri)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "uvas_mesa":
+            response = requests.get(uvas_mesa)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "sem_classificacao":
+            response = requests.get(sem_classificacao)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+
+
+@app.route('/scrape/exportacao/<tipo_e>', methods=['GET'])
+@auth.login_required
+def scrape_exportacao(tipo_e):
+    vinho_de_mesa = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_06'
+    espumantes = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_02&opcao=opt_06'
+    uvas_frescas = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_03&opcao=opt_06'
+    suco_de_uva = 'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_04&opcao=opt_06'
+    if tipo_e not in ['vinho_de_mesa', 'espumantes', 'uvas_frescas', 'suco_de_uva']:
+        return jsonify({'error': 'Tipo de importação inválido'}), 400
+    match tipo_e:
+        case  "vinho_de_mesa":
+            response = requests.get(vinho_de_mesa)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "espumantes":
+            response = requests.get(espumantes)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "uvas_frescas":
+            response = requests.get(uvas_frescas)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
+        case "suco_de_uva":
+            response = requests.get(suco_de_uva)
+
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'tb_base tb_dados'})
+            rows = table.find_all('tr')
+            data = []
+
+            headers = [header.get_text(strip=True) for header in rows[0].find_all('th')]
+
+            for row in rows[1:]:
+                cells = row.find_all('td')
+                item_data = {headers[i]: cell.get_text(strip=True) for i, cell in enumerate(cells)}
+                data.append(item_data)
+
+
+            return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
